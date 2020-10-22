db8e71bddb63d8f02e7932797a79fa8de1adf283
Michael Sharp <msharp@amplience.com>
2020-10-22 16:19:29 +0100

    chore: add SCSS support to app.

--- snowpack.config.js
+++ snowpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     '@snowpack/plugin-svelte',
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-typescript',
+    '@snowpack/plugin-sass',
     [
       '@snowpack/plugin-run-script',
       {cmd: 'svelte-check --output human', watch: '$1 --watch', output: 'stream'},
