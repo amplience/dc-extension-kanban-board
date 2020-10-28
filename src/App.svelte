c50bfcd53f125732f20284a1d1995069a1df78cf
Michael Sharp <msharp@amplience.com>
2020-10-28 16:13:56 +0000

    feat(drag and drop): move hydrate status code into re-callable function and persist SDK params for reuse. WIP as currently using a timeout...

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,13 @@
   import { onMount } from 'svelte';
   import { contentItems } from './services/data';
   import Columns from './components/Columns.svelte';
-  import { init } from './services/dc-extension-client';
+  import { DcExtensionClient, init } from './services/dc-extension-client';
   import { toDcQueryStr } from './utils';
   import type { DcClient } from './services/dc-client';
   import type ContentItem from './services/models/content-item';
 
-  let dcClient: DcClient;
   let hydratedStatuses: any = [];
+  let sdkClient: DcExtensionClient;
 
   function handleConsider(statusId: string, e: CustomEvent<DndEvent>) {
     const statusIndex = hydratedStatuses.findIndex(
@@ -26,37 +26,52 @@
         (item) => item.id === e.detail.info.id
       )[0] as ContentItem;
       const response: ContentItem = await contentItems.updateWorkflowStatus(
-        dcClient,
+        sdkClient.dcClient as DcClient,
         droppedItem,
         statusId
       );
-      droppedItem['lastModifiedDate'] = response['lastModifiedDate'];
-      hydratedStatuses[statusIndex].contentItems.items = listItems.sort(
-        (a: ContentItem, b: ContentItem): number => {
-          const aTicks = new Date(a['lastModifiedDate']).getTime();
-          const bTicks = new Date(b['lastModifiedDate']).getTime();
-          return bTicks - aTicks;
-        }
-      );
+      window.setTimeout(async () => {
+        await loadHydratedStatuses(
+          sdkClient.dcClient as DcClient,
+          sdkClient.statuses,
+          sdkClient.hubId as string,
+          sdkClient.contentRepositoryId as string,
+          sdkClient.folderId as string
+        );
+      }, 2000);
     }
+  }
+
+  async function loadHydratedStatuses(
+    dcClient: DcClient,
+    statuses: any[],
+    hubId: string,
+    contentRepositoryId: string,
+    folderId: string
+  ) {
+    hydratedStatuses = await contentItems.fetchHydrated(
+      dcClient,
+      hubId as string,
+      statuses,
+      {
+        query: toDcQueryStr({
+          status: 'ACTIVE',
+          contentRepositoryId,
+          folderId,
+        }),
+      }
+    );
   }
 
   onMount(async () => {
     try {
-      const client = await init({ debug: true });
-      const { statuses, hubId, contentRepositoryId, folderId } = client;
-      dcClient = client.dcClient as DcClient;
-      hydratedStatuses = await contentItems.fetchHydrated(
-        dcClient,
-        hubId as string,
-        statuses,
-        {
-          query: toDcQueryStr({
-            status: 'ACTIVE',
-            contentRepositoryId,
-            folderId,
-          }),
-        }
+      sdkClient = await init({ debug: true });
+      await loadHydratedStatuses(
+        sdkClient.dcClient as DcClient,
+        sdkClient.statuses,
+        sdkClient.hubId as string,
+        sdkClient.contentRepositoryId as string,
+        sdkClient.folderId as string
       );
     } catch (e) {
       console.error(e);
